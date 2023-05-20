fb780cb66526d35217ca7cfe6bfddbdaf2306555
Michael Bagnall <michael@bagnall.io>
2023-05-20 13:49:37 -0500

    Make authentication token response Guzzle7 compatible

--- src/Provider/FitbitUser.php
+++ src/Provider/FitbitUser.php
@@ -4,43 +4,42 @@ namespace djchen\OAuth2\Client\Provider;
 
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 
-class FitbitUser implements ResourceOwnerInterface
-{
-    /**
-     * @var array
-     */
-    protected $userInfo = [];
+class FitbitUser implements ResourceOwnerInterface {
+  /**
+   * @var array
+   */
+  protected $userInfo = [];
 
-    /**
-     * @param array $response
-     */
-    public function __construct(array $response)
-    {
-        $this->userInfo = $response['user'];
-    }
+  /**
+   * @param GuzzleHttp\Psr7\Response $response
+   */
+  public function __construct(\GuzzleHttp\Psr7\Response $response) {
+    $response = json_decode($response->getBody()->getContents());
+    $this->userInfo = (array) $response->user;
+  }
 
-    public function getId()
-    {
-        return $this->userInfo['encodedId'];
-    }
+  public function getId()
+  {
+      return $this->userInfo['encodedId'];
+  }
 
-    /**
-     * Get the display name.
-     *
-     * @return string
-     */
-    public function getDisplayName()
-    {
-        return $this->userInfo['displayName'];
-    }
+  /**
+   * Get the display name.
+   *
+   * @return string
+   */
+  public function getDisplayName()
+  {
+      return $this->userInfo['displayName'];
+  }
 
-    /**
-     * Get user data as an array.
-     *
-     * @return array
-     */
-    public function toArray()
-    {
-        return $this->userInfo;
-    }
+  /**
+   * Get user data as an array.
+   *
+   * @return array
+   */
+  public function toArray()
+  {
+      return $this->userInfo;
+  }
 }
